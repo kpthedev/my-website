0069fb3bbe3a68b81322a54d1c39d7f1d91ddcf9
KP <115115916+kpthedev@users.noreply.github.com>
2023-02-19 18:43:04 -0500

    Add Cloudflare pages support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://kpthe.dev",
   markdown: {
-    syntaxHighlight: "prism",
+    syntaxHighlight: "prism"
   },
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
